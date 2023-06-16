e7a350c93a25f825db2b5c6441f0d9f11fb83c25
manashotwani16 <123354633+manashotwani16@users.noreply.github.com>
2023-06-16 22:20:51 +0530

    Update bastion_connect.sh

--- projects/bash_networking_security/bastion_connect.sh
+++ projects/bash_networking_security/bastion_connect.sh
@@ -1,1 +1,34 @@
-#!/bin/bash
+    #!/bin/bash
+
+
+if [[ -z "${KEY_PATH}" ]]; then
+  echo "KEY_PATH env var is expected"
+  exit 5
+fi
+
+if [[ -z "${1}" ]]; then
+  echo "Please provide bastion IP address"
+  exit 5
+fi
+
+bastion_ip="${1}"
+private_ip="${2}"
+command="${3}"
+
+
+if [[ -n "${private_ip}" ]]; then
+  ssh -i "${KEY_PATH}" -o ProxyCommand="ssh -i ${KEY_PATH} -W %h:%p ubuntu@${bastion_ip}" ubuntu@${private_ip}
+  exit $?
+fi
+
+ssh -i "${KEY_PATH}" ubuntu@"${bastion_ip}"
+exit $?
+
+if [[ -n "${private_ip}" && -n "${command}" ]]; then
+  ssh -i "${KEY_PATH}" ubuntu@"${bastion_ip}" -t "ssh -i ${KEY_PATH} ubuntu@${private_ip} ${command}"
+  exit $?
+fi
+
+echo "Please provide valid arguments"
+exit 5
+    
